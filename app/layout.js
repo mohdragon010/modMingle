5e7d58467bc0a0b4a60d0619c5e852c93c92f1a4
mohamed ayman <mohammed.ayman152433@gmail.com>
2025-10-28 11:45:47 +0300

    fix: correct component filename casing for Linux build

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
 import ThemeProvider from "./ThemeProvider";
